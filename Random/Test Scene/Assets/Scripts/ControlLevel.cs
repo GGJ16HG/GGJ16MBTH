60c9d82e9af7bcce9fdd0950f7cafbc9e0f73315
Juana Paulina Aguila Hernandez <j.paulina.aguila.hernandez@hotmail.com>
2016-01-30 18:51:35 -0600

    Propuesta de control de nivel
    [#10] Se edita propuesta, para la carga de ítems de altar para ritual.

--- Random/Test Scene/Assets/Scripts/ControlLevel.cs
+++ Random/Test Scene/Assets/Scripts/ControlLevel.cs
@@ -27,34 +27,6 @@ public class ControlLevel : MonoBehaviour
 	public GameObject rosario;
 	public GameObject[] posicionesRosario;
 
-	/*
-	 * Items potenciadores:
-	 * - Capa
-	 * - Pollo
-	 * - Pluma
-	 * - Vial
-	 */
-
-	//Item capa
-	public GameObject capa;
-	public GameObject[] posicionesCapa;
-	public int maxCapa;
-
-	//Item pollo
-	public GameObject pollo;
-	public GameObject[] posicionesPollo;
-	public int maxPollo;
-
-	//Item pluma
-	public GameObject pluma;
-	public GameObject[] posicionesPluma;
-	public int maxPluma;
-
-	//Item vial
-	public GameObject vial;
-	public GameObject[] posicionesVial;
-	public int maxVial;
-
 	//Posicionador de items para ritual
 	void posicionarItemsRitual()
 	{
@@ -82,19 +54,139 @@ public class ControlLevel : MonoBehaviour
 					}
 				}
 
-				Debug.Log("Valor nulo: "+nulo);
-
 				if(nulo != posicionesAguaBendita.Length)
 				{
-					boolean existe = false;
+					bool existe = false;
 					int random = 0;
 					while(existe == false)
 					{
 						random = Random.Range(0,posicionesAguaBendita.Length-1);
 						if(posicionesAguaBendita[random] != null) 
 						{
-							Debug.Log("Ya hay");
 							Instantiate(aguaBendita,posicionesAguaBendita[random].transform.position, Quaternion.identity);
+							existe = true;
+						}
+					}
+				}
+			}
+		}
+
+		//Item biblia
+		if(biblia != null && posicionesBiblia.Length>0)
+		{
+			Debug.Log("Valor no nulo y con valores");
+			if(posicionesBiblia.Length == 1) 
+			{
+				if(posicionesBiblia[0] != null)
+				{
+					Instantiate(biblia,posicionesBiblia[0].transform.position, Quaternion.identity);
+				}
+			} 
+			else 
+			{
+				//Se comprueba que no existan elementos nulos
+				int nulo = 0;
+
+				for (int i = 0; i < posicionesBiblia.Length; i++) 
+				{
+					if(posicionesBiblia[i] == null)
+					{
+						nulo++;
+					}
+				}
+
+				if(nulo != posicionesBiblia.Length)
+				{
+					bool existe = false;
+					int random = 0;
+					while(existe == false)
+					{
+						random = Random.Range(0,posicionesBiblia.Length-1);
+						if(posicionesBiblia[random] != null) 
+						{
+							Instantiate(biblia,posicionesBiblia[random].transform.position, Quaternion.identity);
+							existe = true;
+						}
+					}
+				}
+			}
+		}
+
+		//Item cruz
+		if(cruz != null && posicionesCruz.Length>0)
+		{
+			Debug.Log("Valor no nulo y con valores");
+			if(posicionesCruz.Length == 1) 
+			{
+				if(posicionesCruz[0] != null)
+				{
+					Instantiate(cruz,posicionesCruz[0].transform.position, Quaternion.identity);
+				}
+			} 
+			else 
+			{
+				//Se comprueba que no existan elementos nulos
+				int nulo = 0;
+
+				for (int i = 0; i < posicionesCruz.Length; i++) 
+				{
+					if(posicionesCruz[i] == null)
+					{
+						nulo++;
+					}
+				}
+
+				if(nulo != posicionesCruz.Length)
+				{
+					bool existe = false;
+					int random = 0;
+					while(existe == false)
+					{
+						random = Random.Range(0,posicionesCruz.Length-1);
+						if(posicionesCruz[random] != null) 
+						{
+							Instantiate(cruz,posicionesCruz[random].transform.position, Quaternion.identity);
+							existe = true;
+						}
+					}
+				}
+			}
+		}
+
+		//Item rosario
+		if(rosario != null && posicionesRosario.Length>0)
+		{
+			Debug.Log("Valor no nulo y con valores");
+			if(posicionesCruz.Length == 1) 
+			{
+				if(posicionesCruz[0] != null)
+				{
+					Instantiate(rosario,posicionesRosario[0].transform.position, Quaternion.identity);
+				}
+			} 
+			else 
+			{
+				//Se comprueba que no existan elementos nulos
+				int nulo = 0;
+
+				for (int i = 0; i < posicionesRosario.Length; i++) 
+				{
+					if(posicionesRosario[i] == null)
+					{
+						nulo++;
+					}
+				}
+
+				if(nulo != posicionesRosario.Length)
+				{
+					bool existe = false;
+					int random = 0;
+					while(existe == false)
+					{
+						random = Random.Range(0,posicionesRosario.Length-1);
+						if(posicionesCruz[random] != null) 
+						{
+							Instantiate(rosario,posicionesRosario[random].transform.position, Quaternion.identity);
 							existe = true;
 						}
 					}
@@ -103,6 +195,34 @@ public class ControlLevel : MonoBehaviour
 		}
 			
 	}
+
+	/*
+	 * Items potenciadores:
+	 * - Capa
+	 * - Pollo
+	 * - Pluma
+	 * - Vial
+	 */
+
+	//Item capa
+	public GameObject capa;
+	public GameObject[] posicionesCapa;
+	public int maxCapa;
+
+	//Item pollo
+	public GameObject pollo;
+	public GameObject[] posicionesPollo;
+	public int maxPollo;
+
+	//Item pluma
+	public GameObject pluma;
+	public GameObject[] posicionesPluma;
+	public int maxPluma;
+
+	//Item vial
+	public GameObject vial;
+	public GameObject[] posicionesVial;
+	public int maxVial;
 
 	// Use this for initialization
 	void Start () 
