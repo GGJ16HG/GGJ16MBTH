e095fe23fc36a35ee406cc7404621c55a7d0b97c
Juana Paulina Aguila Hernandez <j.paulina.aguila.hernandez@hotmail.com>
2016-01-31 03:17:01 -0600

    Respaldo

---
+++ Random/Test Scene/Assets/Scripts/ItemPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPosition : MonoBehaviour 
+{
+
+	private int position = -1;
+
+	public void setOcupado(int position)
+	{
+		this.position = position;
+	}
+
+	public int getOcupado()
+	{
+		return this.position;
+	}
+}
